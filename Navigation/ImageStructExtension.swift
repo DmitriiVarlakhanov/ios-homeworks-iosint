746e54834c5da5aa979685802db12ef5e0f7fdf3
Dmitrii Varlakhanov <dmitriivarlakhanov@gmail.com>
2025-10-02 20:38:10 +0300

    Final version before checking on feature-iosint-60/2

--- Navigation/ImageStructExtension.swift
+++ Navigation/ImageStructExtension.swift
@@ -10,27 +10,6 @@ import Foundation
 extension Image {
 
     static func make() -> [Image] {
-        return [
-            Image(name: "PhotoCollectionView1"),
-            Image(name: "PhotoCollectionView2"),
-            Image(name: "PhotoCollectionView3"),
-            Image(name: "PhotoCollectionView4"),
-            Image(name: "PhotoCollectionView5"),
-            Image(name: "PhotoCollectionView6"),
-            Image(name: "PhotoCollectionView7"),
-            Image(name: "PhotoCollectionView8"),
-            Image(name: "PhotoCollectionView9"),
-            Image(name: "PhotoCollectionView10"),
-            Image(name: "PhotoCollectionView11"),
-            Image(name: "PhotoCollectionView12"),
-            Image(name: "PhotoCollectionView13"),
-            Image(name: "PhotoCollectionView14"),
-            Image(name: "PhotoCollectionView15"),
-            Image(name: "PhotoCollectionView16"),
-            Image(name: "PhotoCollectionView17"),
-            Image(name: "PhotoCollectionView18"),
-            Image(name: "PhotoCollectionView19"),
-            Image(name: "PhotoCollectionView20")
-        ]
+        return (1...20).map { Image(name: "PhotoCollectionView\($0)") }
     }
 }
